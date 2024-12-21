44630e5bf3313ba349a41b232c26db93cde147ba
Rajat Soni <98265341+rajat-sonigit@users.noreply.github.com>
2024-12-21 13:43:17 +0530

    Update login.dart

--- lib/login.dart
+++ lib/login.dart
@@ -5,7 +5,6 @@ import 'package:interproj/signup.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
